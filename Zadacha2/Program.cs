36f683a8200c61af874229f81b4e183bf4ee98ad
Haru <harumolodec@mail.ru>
2022-08-06 13:05:51 +0700

    Zadacha2 END

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GetArray(int size, int minRandom, int maxRandom)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(minRandom,maxRandom + 1);
+    }
+    return result;
+}
+
+int[] array = GetArray(5,0,100);
+Console.WriteLine($"Массив: [ {String.Join(", ", array)} ]");
+
+int sum = 0;
+
+for (int i = 0; i < array.Length; i+=2)
+{
+    sum += array[i];
+}
+
+Console.WriteLine($"Сумма элементов на нечётных позициях = {sum} ");
+
+
